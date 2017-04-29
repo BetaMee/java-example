cc61cb53bcc148aab31340dd2f069beba8784d2b
betamee <gongxq95@gmail.com>
2017-04-29 17:35:35 +0800

    新建安卓

--- form/src/com/example/servlet/Servlet.java
+++ form/src/com/example/servlet/Servlet.java
@@ -1,13 +1,41 @@
+package com.example.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
 /**
  * Created by Betamee on 2017/4/26.
  */
-@javax.servlet.annotation.WebServlet(name = "Servlet")
-public class Servlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+@WebServlet(name = "Servlet",urlPatterns = {"/form"})
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 设置响应内容类型
+        response.setContentType("text/html;charset=UTF-8");
 
+        PrintWriter out = response.getWriter();
+        String title = "使用 GET 方法读取表单数据";
+        // 处理中文
+        String name =new String(request.getParameter("name").getBytes("UTF-8"),"UTF-8");
+        String docType = "<!DOCTYPE html> \n";
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<body bgcolor=\"#f0f0f0\">\n" +
+                "<h1 align=\"center\">" + title + "</h1>\n" +
+                "<ul>\n" +
+                "  <li><b>站点名</b>："
+                + name + "\n" +
+                "  <li><b>网址</b>："
+                + request.getParameter("url") + "\n" +
+                "</ul>\n" +
+                "</body></html>");
     }
 }

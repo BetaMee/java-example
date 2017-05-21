6618aa1d4ffd1b465d522404f8c6c6e7b519f000
betamee <gongxq95@gmail.com>
2017-05-22 00:14:00 +0800

    重新新建Java-web项目

---
+++ RailwaySys/src/Railway/Manager.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Betamee on 2017/5/21.
+ */
+@javax.servlet.annotation.WebServlet(name = "Manager")
+public class Manager extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+}
---
+++ RailwaySys/web/build/js/hello.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Betamee on 2017/5/21.
+ */

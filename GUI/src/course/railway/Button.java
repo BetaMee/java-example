791e31bd70653a7451a2ad2e193c1b438842d5ed
betamee <gongxq95@gmail.com>
2017-05-21 20:43:56 +0800

    更新项目，添加铁路管理系统

--- GUI/src/course/railway/Button.java
+++
@@ -1,7 +0,0 @@
-package course.railway;
-
-/**
- * Created by Betamee on 2017/4/26.
- */
-public class Button {
-}
---
+++ RailwaySys/src/Railway/Customer.java
@@ -0,0 +1,32 @@
+package Railway;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+/**
+ * Created by Betamee on 2017/5/21.
+ */
+public class Customer extends javax.servlet.http.HttpServlet {
+    private String message = "hhhh";
+
+    public void init() throws ServletException{
+
+    }
+
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        // 设置响应的数据
+        response.setContentType("text/html");
+        // 处理逻辑
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + "</h1>");
+    }
+    public void destroy() {
+
+    }
+}

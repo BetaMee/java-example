b1d917c6540795ebc95bc52b3f9353ab9222dd55
betamee <gongxq95@gmail.com>
2017-06-07 00:13:12 +0800

    更新

--- RailwaySys/src/Railway/Ticket.java
+++ RailwaySys/src/Railway/Ticket.java
@@ -71,6 +71,7 @@ public class Ticket extends HttpServlet {
                 String start_city = rs.getString("start_city");
                 String end_city = rs.getString("end_city");
                 Integer tickets = rs.getInt("tickets");
+                Integer price =  rs.getInt("price");
                 try {
                     jsonObj.put("id",id)
                             .put("train_name",train_name)
@@ -78,7 +79,8 @@ public class Ticket extends HttpServlet {
                             .put("start_time",start_time)
                             .put("start_city",start_city)
                             .put("end_city",end_city)
-                            .put("tickets",tickets);
+                            .put("tickets",tickets)
+                            .put("price",price);
                     // 放入数组中
                     jsonArr.put(jsonObj);
                 }catch (JSONException je){
